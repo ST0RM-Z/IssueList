f65b20a51e470573e822dd88d3559a3bf65d01f1
Dhairya Arya <stromcyclone1921@gmail.com>
2022-09-19 19:40:16 -0400

    useEffect

--- public/app.js
+++ public/app.js
@@ -3,41 +3,73 @@ const IssueFilter = () => {
 };
 
 const IssueTable = () => {
-  const rowStyle = {
-    "border": "1px solid"
-  };
-  const issues = [{
+  // const rowStyle = {
+  //   "border": "1px solid"
+  // };
+  // const tempIssues = [];
+  const tempIssues = [{
+    Id: 1,
+    Owner: "Person-B",
+    Created: new Date('2022-09-19'),
+    Due: new Date("2022-09-21"),
+    Status: "Assigned",
+    Title: "this is 1st issue"
+  }, {
+    Id: 2,
+    Owner: "Person-A",
+    Created: new Date('2022-09-18'),
+    Due: new Date("2022-09-22"),
+    Status: "Resolved",
+    Title: "this is 2nd issue"
+  }, {
+    Id: 3,
+    Owner: "Person-A",
+    Created: new Date('2022-09-18'),
+    Due: new Date("2022-09-21"),
+    Status: "Assigned",
+    Title: "this is 3rd issue"
+  }, {
+    Id: 4,
+    Owner: "Person-A",
+    Created: new Date('2022-09-18'),
+    Due: new Date("2022-09-21"),
+    Status: "Assigned",
+    Title: "this is 4th issue"
+  }, {
     Id: 5,
+    Owner: "Person-A",
+    Created: new Date('2022-09-18'),
+    Due: new Date("2022-09-21"),
+    Status: "Assigned",
     Title: "this is 5th issue"
   }, {
     Id: 6,
+    Owner: "Person-A",
+    Created: new Date('2022-09-18'),
+    Due: new Date("2022-09-21"),
+    Status: "Assigned",
     Title: "this is 6th issue"
   }];
+  const [issues, setIssues] = React.useState([]); // const issues = [{ Id: 1, Owner: "Person-B", Created: new Date('2022-09-19'), Due: new Date("2022-09-21"), Status: "Assigned", Title: "this is 1st issue" }, { Id: 2, Owner: "Person-A", Created: new Date('2022-09-18'), Due: new Date("2022-09-22"), Status: "Resolved", Title: "this is 2nd issue" }, { Id: 3, Owner: "Person-A", Created: new Date('2022-09-18'), Due: new Date("2022-09-21"), Status: "Assigned", Title: "this is 3rd issue" }, { Id: 4, Owner: "Person-A", Created: new Date('2022-09-18'), Due: new Date("2022-09-21"), Status: "Assigned", Title: "this is 4th issue" }, { Id: 5, Owner: "Person-A", Created: new Date('2022-09-18'), Due: new Date("2022-09-21"), Status: "Assigned", Title: "this is 5th issue" }, { Id: 6, Owner: "Person-A", Created: new Date('2022-09-18'), Due: new Date("2022-09-21"), Status: "Assigned", Title: "this is 6th issue" }]
+
+  setTimeout(() => {
+    setIssues(tempIssues);
+  }, 3000);
 
   const IssueRow = props => {
-    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
-      style: props.rowStyle
-    }, props.Id), /*#__PURE__*/React.createElement("td", {
-      style: props.rowStyle
-    }, props.Title)));
+    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, props.Id), /*#__PURE__*/React.createElement("td", null, props.Title), /*#__PURE__*/React.createElement("td", null, props.Owner), /*#__PURE__*/React.createElement("td", null, props.Created.toDateString()), /*#__PURE__*/React.createElement("td", null, props.Due.toDateString()), /*#__PURE__*/React.createElement("td", null, props.Status)));
   };
 
   const issueRow = issues.map((issue, key) => /*#__PURE__*/React.createElement(IssueRow, {
-    rowStyle: rowStyle,
     Id: issue.Id,
-    Title: issue.Title
+    Title: issue.Title,
+    Owner: issue.Owner,
+    Created: issue.Created,
+    Due: issue.Due,
+    Status: issue.Status,
+    key: key
   }));
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Issue Table"), /*#__PURE__*/React.createElement("table", {
-    style: rowStyle
-  }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "ID"), /*#__PURE__*/React.createElement("th", null, "table header"))), /*#__PURE__*/React.createElement("tbody", null, /*#__PURE__*/React.createElement(IssueRow, {
-    rowStyle: rowStyle,
-    Id: 3,
-    Title: "This is third issue"
-  }), /*#__PURE__*/React.createElement(IssueRow, {
-    rowStyle: rowStyle,
-    Id: 4,
-    Title: "This is Fourth issue"
-  }), issueRow)));
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Issue Table"), /*#__PURE__*/React.createElement("table", null, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "ID"), /*#__PURE__*/React.createElement("th", null, "Title"), /*#__PURE__*/React.createElement("th", null, "Owner"), /*#__PURE__*/React.createElement("th", null, "Created"), /*#__PURE__*/React.createElement("th", null, "Due"), /*#__PURE__*/React.createElement("th", null, "Status"))), /*#__PURE__*/React.createElement("tbody", null, issueRow)));
 };
 
 const AddIssue = () => {
--- src/app.jsx
+++ src/app.jsx
@@ -3,40 +3,58 @@ const IssueFilter = () => {
 };
 
 const IssueTable = () => {
-  const rowStyle = {
-    "border": "1px solid"
-  };
-  const issues = [{ Id: 5, Title: "this is 5th issue" }, { Id: 6, Title: "this is 6th issue" }]
+  // const rowStyle = {
+  //   "border": "1px solid"
+  // };
+  // const tempIssues = [];
+  const tempIssues = [{ Id: 1, Owner: "Person-B", Created: new Date('2022-09-19'), Due: new Date("2022-09-21"), Status: "Assigned", Title: "this is 1st issue" }, { Id: 2, Owner: "Person-A", Created: new Date('2022-09-18'), Due: new Date("2022-09-22"), Status: "Resolved", Title: "this is 2nd issue" }, { Id: 3, Owner: "Person-A", Created: new Date('2022-09-18'), Due: new Date("2022-09-21"), Status: "Assigned", Title: "this is 3rd issue" }, { Id: 4, Owner: "Person-A", Created: new Date('2022-09-18'), Due: new Date("2022-09-21"), Status: "Assigned", Title: "this is 4th issue" }, { Id: 5, Owner: "Person-A", Created: new Date('2022-09-18'), Due: new Date("2022-09-21"), Status: "Assigned", Title: "this is 5th issue" }, { Id: 6, Owner: "Person-A", Created: new Date('2022-09-18'), Due: new Date("2022-09-21"), Status: "Assigned", Title: "this is 6th issue" }]
 
+  const [issues, setIssues] = React.useState([]);
+
+  // const issues = [{ Id: 1, Owner: "Person-B", Created: new Date('2022-09-19'), Due: new Date("2022-09-21"), Status: "Assigned", Title: "this is 1st issue" }, { Id: 2, Owner: "Person-A", Created: new Date('2022-09-18'), Due: new Date("2022-09-22"), Status: "Resolved", Title: "this is 2nd issue" }, { Id: 3, Owner: "Person-A", Created: new Date('2022-09-18'), Due: new Date("2022-09-21"), Status: "Assigned", Title: "this is 3rd issue" }, { Id: 4, Owner: "Person-A", Created: new Date('2022-09-18'), Due: new Date("2022-09-21"), Status: "Assigned", Title: "this is 4th issue" }, { Id: 5, Owner: "Person-A", Created: new Date('2022-09-18'), Due: new Date("2022-09-21"), Status: "Assigned", Title: "this is 5th issue" }, { Id: 6, Owner: "Person-A", Created: new Date('2022-09-18'), Due: new Date("2022-09-21"), Status: "Assigned", Title: "this is 6th issue" }]
+  setTimeout(() => {
+    setIssues(tempIssues);
+  }, 3000);
 
   const IssueRow = (props) => {
     return (<>
       <tr>
-        <td style={props.rowStyle}>{props.Id}</td>
-        <td style={props.rowStyle}>{props.Title}</td>
+        <td>{props.Id}</td>
+        <td>{props.Title}</td>
+        <td>{props.Owner}</td>
+        <td>{props.Created.toDateString()}</td>
+
+        <td>{props.Due.toDateString()}</td>
+
+
+        <td>{props.Status}</td>
+
 
       </tr>
     </>)
   }
 
   const issueRow = issues.map((issue, key) => (
-    <IssueRow rowStyle={rowStyle} Id={issue.Id} Title={issue.Title} />
+    <IssueRow Id={issue.Id} Title={issue.Title} Owner={issue.Owner} Created={issue.Created} Due={issue.Due} Status={issue.Status} key={key} />
   ))
 
   return (<>
     <h1>Issue Table</h1>
-    <table style={rowStyle}>
+    <table >
       <thead>
         <tr>
           <th>ID</th>
 
-          <th>table header</th>
+          <th>Title</th>
+          <th>Owner</th>
+          <th>Created</th>
+          <th>Due</th>
+          <th>Status</th>
+
+
         </tr>
       </thead>
       <tbody>
-
-        <IssueRow rowStyle={rowStyle} Id={3} Title={"This is third issue"} />
-        <IssueRow rowStyle={rowStyle} Id={4} Title={"This is Fourth issue"} />
         {issueRow}
       </tbody>
     </table>
